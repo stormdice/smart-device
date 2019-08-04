0fc4d13e789d689151aac9bfbc52b68752f06518
stormdice <stormdice23@gmail.com>
2019-08-05 04:31:25 +0500

    добавляет раздел 'О компании'

--- build/js/script.js
+++ build/js/script.js
@@ -1,1 +1,7 @@
 'use strict';
+const link = document.querySelector('.js-scroll-down');
+
+link.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  window.scroll({ top: 2500, left: 0, behavior: 'smooth' });
+});
