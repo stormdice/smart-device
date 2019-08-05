6a6b407f3477f8c13ca1a7d368d3fed6b1c122d0
stormdice <stormdice23@gmail.com>
2019-08-05 13:49:54 +0500

    добавил файлы для gh-pages

--- source/js/script.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const linkScrollDown = document.querySelector('.js-scroll-down');
-
-linkScrollDown.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  window.scroll({ top: 900, left: 0, behavior: 'smooth' });
-});
