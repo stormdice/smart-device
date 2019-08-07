5e5e233bd58dead0a2b03f3d2d0faa9cd0aeee8a
stormdice <stormdice23@gmail.com>
2019-08-07 15:16:30 +0500

    добавляет футер

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
 'use strict';
-const linkScrollDown = document.querySelector('.js-scroll-down');
 
-linkScrollDown.addEventListener('click', function(evt) {
+var tabletWidth = 770;
+
+if (screen.width < tabletWidth) {
+  var footerContentList = document.querySelectorAll('.js-footer-list');
+  footerContentList.forEach(function (list) {
+    list.addEventListener('click', function () {
+      list.classList.toggle('footer__content--height');
+    });
+  });
+}
+
+'use strict';
+
+var linkScrollDown = document.querySelector('.js-scroll-down');
+linkScrollDown.addEventListener('click', function (evt) {
   evt.preventDefault();
-  window.scroll({ top: 900, left: 0, behavior: 'smooth' });
+  window.scroll({
+    top: 900,
+    left: 0,
+    behavior: 'smooth'
+  });
 });
