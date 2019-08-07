ce1ab3081d53fdec69c9167e621e68b6fc92b7de
stormdice <stormdice23@gmail.com>
2019-08-07 18:49:37 +0500

    правит скрипт аккордиона

--- source/js/footer.js
+++ source/js/footer.js
@@ -1,43 +1,13 @@
 'use strict';
 
-const DEBOUNCE_INTERVAL = 500;
+const tabletWidth = 770;
 
-/**
- * устранение дребезга
- * @param {function} callBack - коллбек функция
- * @return {function}
- */
-const debounce = function(callBack) {
-  let lastTimeout = null;
+if (screen.width < tabletWidth) {
+  const footerContentList = document.querySelectorAll('.js-footer-list');
 
-  return function() {
-    const parameters = arguments;
-
-    if (lastTimeout) {
-      window.clearTimeout(lastTimeout);
-    }
-    lastTimeout = window.setTimeout(function() {
-      callBack.apply(null, parameters);
-    }, DEBOUNCE_INTERVAL);
-  };
-};
-
-/**
- * показывает/скрывает список контента в футере
- */
-const showFooterContentList = () => {
-  const tabletWidth = 770;
-
-  if (screen.width < tabletWidth) {
-    console.log(screen.width);
-    const footerContentList = document.querySelectorAll('.js-footer-list');
-
-    footerContentList.forEach(list => {
-      list.addEventListener('click', () => {
-        list.classList.toggle('footer__content--height');
-      });
+  footerContentList.forEach(list => {
+    list.addEventListener('click', () => {
+      list.classList.toggle('footer__content--height');
     });
-  }
-};
-
-window.addEventListener('resize', debounce(showFooterContentList));
+  });
+}
