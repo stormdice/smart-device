874ab3881899678386321131b37f7ba8bdf798e8
stormdice <stormdice23@gmail.com>
2019-08-07 18:50:55 +0500

    правит скрипт аккордиона

--- js/script.js
+++ js/script.js
@@ -1,46 +1,16 @@
 'use strict';
 
-var DEBOUNCE_INTERVAL = 500;
-/**
- * устранение дребезга
- * @param {function} callBack - коллбек функция
- * @return {function}
- */
+var tabletWidth = 770;
 
-var debounce = function debounce(callBack) {
-  var lastTimeout = null;
-  return function () {
-    var parameters = arguments;
-
-    if (lastTimeout) {
-      window.clearTimeout(lastTimeout);
-    }
-
-    lastTimeout = window.setTimeout(function () {
-      callBack.apply(null, parameters);
-    }, DEBOUNCE_INTERVAL);
-  };
-};
-/**
- * показывает/скрывает список контента в футере
- */
-
-
-var showFooterContentList = function showFooterContentList() {
-  var tabletWidth = 770;
-
-  if (screen.width < tabletWidth) {
-    console.log(screen.width);
-    var footerContentList = document.querySelectorAll('.js-footer-list');
-    footerContentList.forEach(function (list) {
-      list.addEventListener('click', function () {
-        list.classList.toggle('footer__content--height');
-      });
+if (screen.width < tabletWidth) {
+  var footerContentList = document.querySelectorAll('.js-footer-list');
+  footerContentList.forEach(function (list) {
+    list.addEventListener('click', function () {
+      list.classList.toggle('footer__content--height');
     });
-  }
-};
+  });
+}
 
-window.addEventListener('resize', debounce(showFooterContentList));
 'use strict';
 
 var linkScrollDown = document.querySelector('.js-scroll-down');
