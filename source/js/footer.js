9a37f9d2bd76a93c7937dba2c890ffea48fca778
stormdice <stormdice23@gmail.com>
2019-08-07 15:57:40 +0500

    оптимизирует js для показа/скрытия списков в футере на мобильной версии

--- source/js/footer.js
+++ source/js/footer.js
@@ -1,13 +1,43 @@
 'use strict';
 
-const tabletWidth = 770;
+const DEBOUNCE_INTERVAL = 500;
 
-if (screen.width < tabletWidth) {
-  const footerContentList = document.querySelectorAll('.js-footer-list');
+/**
+ * устранение дребезга
+ * @param {function} callBack - коллбек функция
+ * @return {function}
+ */
+const debounce = function(callBack) {
+  let lastTimeout = null;
 
-  footerContentList.forEach(list => {
-    list.addEventListener('click', () => {
-      list.classList.toggle('footer__content--height');
+  return function() {
+    const parameters = arguments;
+
+    if (lastTimeout) {
+      window.clearTimeout(lastTimeout);
+    }
+    lastTimeout = window.setTimeout(function() {
+      callBack.apply(null, parameters);
+    }, DEBOUNCE_INTERVAL);
+  };
+};
+
+/**
+ * показывает/скрывает список контента в футере
+ */
+const showFooterContentList = () => {
+  const tabletWidth = 770;
+
+  if (screen.width < tabletWidth) {
+    console.log(screen.width);
+    const footerContentList = document.querySelectorAll('.js-footer-list');
+
+    footerContentList.forEach(list => {
+      list.addEventListener('click', () => {
+        list.classList.toggle('footer__content--height');
+      });
     });
-  });
-}
+  }
+};
+
+window.addEventListener('resize', debounce(showFooterContentList));
